6f71c880bd83f71ee38b16ce78eeee506ea57e18
LucidRavings <cybernerd82@yahoo.com>
2023-06-05 05:36:29 -0600

    Completed part3

--- crud.py
+++ crud.py
@@ -13,6 +13,14 @@ def create_user(email, password):
         )
     return user
 
+def get_users():
+    """Return all users."""
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+    return User.query.get(user_id)
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -23,6 +31,14 @@ def create_movie(title, overview, release_date, poster_path):
         poster_path=poster_path,
     )
     return movie
+
+def get_movies():
+    """Return all movies."""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Return a movie by primary key."""
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
     """Create and return a new rating."""
